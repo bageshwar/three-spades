da3cdfa3baf04b23f340d1e5e8ca313b3ab36f12
hunnynarain@gmail.com <hunnynarain@gmail.com@22392f04-415a-e5f5-604b-434b86a56ca0>
2011-08-06 03:38:32 +0000

    Fixing NPE's

--- trunk/src/org/techno/blackthree/client/Client.java
+++ trunk/src/org/techno/blackthree/client/Client.java
@@ -28,21 +28,20 @@ public class Client implements Runnable {
 	 * This monitor blocks any thread to access this thread's info until the
 	 * stream and vital parameters have been successfully initialized.
 	 * */
-	
+
 	private boolean monitor = false;
 	private ObjectInputStream input;
 	private ObjectOutputStream output;
-	
+
 	private boolean connectionOK = false;
 	private Player player;
-	
-	private Integer maxBid;
-	
-	private int tempBidForTest=0;
 
-	
+	private Integer maxBid = 0;
+
+	private int tempBidForTest = 0;
+
 	private boolean tired = false;
-	
+
 	public boolean isTired() {
 		return tired;
 	}
@@ -51,44 +50,48 @@ public class Client implements Runnable {
 		this.tired = tired;
 	}
 
-	public static void main(String s[]) {		
-		
-		Client client = null;
+	public static void main(String s[]) {
 
-		if (s == null) {
-			System.out.println("Host/port missing");
-			System.exit(-1);
-		} else if (s.length == 1) {
-			try {
-				client = new Client(s[0]);
-			} catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} else
+		for (int i = 0; i < 4; i++) {
 
-		if (s.length == 2) {
-			try {
-				client = new Client(s[0], Integer.parseInt(s[1]));
-			} catch (NumberFormatException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			Client client = null;
+
+			if (s == null) {
+				System.out.println("Host/port missing");
+				System.exit(-1);
+			} else if (s.length == 1) {
+				try {
+					client = new Client(s[0]);
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else
+
+			if (s.length == 2) {
+				try {
+					client = new Client(s[0], Integer.parseInt(s[1]));
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else {
+				System.out
+						.println("Invalid Number of parameters:  Client host port");
 			}
-		} else {
-			System.out.println("Invalid Number of parameters:  Client host port");
+
+			new Thread(client).start();
+
 		}
-
-		new Thread(client).start();
-
 	}
 
 	private Client(String host) throws UnknownHostException, IOException {
@@ -97,35 +100,31 @@ public class Client implements Runnable {
 
 	}
 
-	private Client(String host, int port) throws UnknownHostException, IOException {
+	private Client(String host, int port) throws UnknownHostException,
+			IOException {
 		clientSocket = new Socket(host, port);
-		player = new Player("Player# "+(int)(Math.random()*200));
+		player = new Player("Player# " + (int) (Math.random() * 200));
 	}
 
 	@Override
 	public void run() {
-		 
+
 		try {
 			initStreams();
-			
+
 			/**
-			 * Flow
-			 * 1. Wait for OK
-			 * 2. Send player details
-			 * 3. Get Players update
-			 * --------
-			 * 4. Wait for round start
-			 * 5. Get Card details
+			 * Flow 1. Wait for OK 2. Send player details 3. Get Players update
+			 * -------- 4. Wait for round start 5. Get Card details
 			 * 
 			 * */
-			
-				while(!isTired()){
-					Thread.sleep(200);			
-					String s = (String) input.readObject();
-					handleInput(s);
-				}
-				
-			
+
+			while (!isTired()) {
+				Thread.sleep(200);
+				String s = (String) input.readObject();
+				System.out.println(player.getName()+" >> "+s);
+				handleInput(s);
+			}
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -136,142 +135,151 @@ public class Client implements Runnable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	/**
 	 * The actual orchestrator for client
-	 * @throws ClassNotFoundException 
-	 * @throws IOException 
-	 * @throws InterruptedException 
+	 * 
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 * @throws InterruptedException
 	 * */
-	private void handleInput(String s) throws IOException, ClassNotFoundException, InterruptedException {
-		
-		if(s==null)
-			return; //do nothing
-		
-		if(s.equals(Codes.GIVE_PLAYER))
+	private void handleInput(String s) throws IOException,
+			ClassNotFoundException, InterruptedException {
+
+		if (s == null)
+			return; // do nothing
+
+		if (s.equals(Codes.GIVE_PLAYER))
 			initPlayerDetails();
 		else if (s.equals(Codes.PLAYER_UPDATE))
 			updatePlayersDetails();
-		else if(s.equals(Codes.BID))
+		else if (s.equals(Codes.BID))
 			placeBid();
-		else if(s.equals(Codes.BID_UPDATE))
+		else if (s.equals(Codes.BID_UPDATE))
 			updateBidDetails();
-		else if(s.equals(Codes.GAME_OVER))
+		else if (s.equals(Codes.GAME_OVER))
 			gameOver();
-		else if(s.equals(Codes.ADHOC_MESSAGE))
+		else if (s.equals(Codes.ADHOC_MESSAGE))
 			processAdhocMessage();
-		else if(s.equals(Codes.ACCEPT_HAND))
+		else if (s.equals(Codes.ACCEPT_HAND))
 			acceptDeal();
 	}
 
 	private void acceptDeal() throws IOException, ClassNotFoundException {
-		ArrayList<Card> deal = (ArrayList<Card>) input.readObject();
+		Object o =input.readObject();		
+		ArrayList<Card> deal = (ArrayList<Card>) o;
 		player.setCards(deal);
-		System.out.println("Deal "+deal);		
-		
-		
+		System.out.println("Deal " + deal);
+
 	}
 
-	private void processAdhocMessage() throws IOException, ClassNotFoundException {
+	private void processAdhocMessage() throws IOException,
+			ClassNotFoundException {
 		String msg = (String) input.readObject();
-		System.out.println("<<ServerCast>> "+msg);
-		
+		System.out.println(player.getName()+" <<ServerCast>> " + msg);
+
 	}
 
 	private void gameOver() {
-		//process the game over update
+		// process the game over update
 		setTired(true);
-		
+
 	}
 
 	private void updateBidDetails() throws IOException, ClassNotFoundException {
-		System.out.println("New King >> "+input.readObject());
-		maxBid =  (Integer) input.readObject();
-		System.out.println("His Bid >> "+maxBid);
-		
+		System.out.println("New King >> " + input.readObject());
+		maxBid = (Integer) input.readObject();
+		System.out.println("His Bid >> " + maxBid);
+
 	}
 
-	private void placeBid() throws IOException, InterruptedException, ClassNotFoundException {
-		Integer bid = (int)(Math.random()*100);
-		bid = bid+150;
-		
-		if(tempBidForTest++ == 2)
-			bid=-1;
-		
-		if(bid<maxBid)
-			bid=-1;
-		
-		System.out.println("Me, "+this.player.toString()+" am placing a bid of "+bid);
-		
+	private void placeBid() throws IOException, InterruptedException,
+			ClassNotFoundException {
+		Integer bid = (int) (Math.random() * 100);
+		bid = bid + 150;
+
+		if (tempBidForTest++ == 2)
+			bid = -1;
+
+		if (bid < maxBid)
+			bid = -1;
+
+		System.out.println("Me, " + this.player.toString()
+				+ " am placing a bid of " + bid);
+
 		output.writeObject(bid);
 		waitForOK();
-		
+
 	}
 
-	
 	/**
 	 * Utility method to wait for the OK status.
-	 * @throws ClassNotFoundException 
-	 * @throws IOException 
-	 * @throws InterruptedException 
+	 * 
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 * @throws InterruptedException
 	 * */
-	private void waitForOK() throws IOException, ClassNotFoundException, InterruptedException{
-		String s ;
+	private void waitForOK() throws IOException, ClassNotFoundException,
+			InterruptedException {
+		String s;
 		s = (String) input.readObject();
-		while(!s.equals(Codes.OK)){
+		while (!s.equals(Codes.OK)) {
 			Thread.sleep(200);
 			s = (String) input.readObject();
 		}
 	}
-	
+
 	/**
-	 * Update player details.
-	 *  A single string with the name of the player is expected.
-	 * @throws ClassNotFoundException 
-	 * @throws IOException 
+	 * Update player details. A single string with the name of the player is
+	 * expected.
+	 * 
+	 * @throws ClassNotFoundException
+	 * @throws IOException
 	 * */
-	private void updatePlayersDetails() throws IOException, ClassNotFoundException {
-		
+	private void updatePlayersDetails() throws IOException,
+			ClassNotFoundException {
+
 		String p = (String) input.readObject();
-		
-		System.out.println(p+" has joined the game.");
-		
-		//utput.wriite
-		//no need to notify server of ok
-		
+
+		System.out.println(p + " has joined the game.");
+
+		// utput.wriite
+		// no need to notify server of ok
+
 	}
 
-	private void initPlayerDetails() throws IOException, ClassNotFoundException, InterruptedException {
-		
-			output.writeObject(this.player);
-			output.flush();
-			waitForOK();
-			
-		
+	private void initPlayerDetails() throws IOException,
+			ClassNotFoundException, InterruptedException {
+
+		output.writeObject(this.player);
+		output.flush();
+		waitForOK();
+
 	}
 
-	private void initStreams() throws IOException, ClassNotFoundException, InterruptedException {
+	private void initStreams() throws IOException, ClassNotFoundException,
+			InterruptedException {
 
 		System.out.println("Initing Client streams...");
 		input = new ObjectInputStream(clientSocket.getInputStream());
 		output = new ObjectOutputStream(clientSocket.getOutputStream());
 
-		
-		 
-		//Object s = input.readObject();
-		//System.out.println("read "+s);
-		/*if (Codes.OK.equalsIgnoreCase(s.toString()))
-			System.out.println("Connection Successfull");*/
-		
+		// Object s = input.readObject();
+		// System.out.println("read "+s);
+		/*
+		 * if (Codes.OK.equalsIgnoreCase(s.toString()))
+		 * System.out.println("Connection Successfull");
+		 */
+
 		waitForOK();
 		System.out.println("Connection Successful at client");
 		output.writeObject(Codes.OK);
 		output.flush();
 
 		System.out.println("Connection Successfull at server");
-		System.out.println("Hi, this is "+player.getName());
-		
+		System.out.println("Hi, this is " + player.getName());
 	}
+
 }
